e7045e5c3959a5cf29282e9716b22d1baa1c5682
Oliver Forral <intrepion@gmail.com>
2020-11-22 17:52:30 -0800

    Create the Instructor controller and views

--- ContosoUniversity/Controllers/CoursesController.cs
+++ ContosoUniversity/Controllers/CoursesController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Data;
-using ContosoUniversity.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Controllers
 {
